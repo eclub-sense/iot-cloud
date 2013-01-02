845146ef02689f4b66009b275ff1c4efe704d7a4
Filip Kolarik <filip26@gmail.com>
2013-01-02 23:27:14 +0100

    fix readToken() method

--- src/main/java/cz/cvut/felk/rest/todo/http/HttpLang.java
+++ src/main/java/cz/cvut/felk/rest/todo/http/HttpLang.java
@@ -167,7 +167,7 @@ public class HttpLang {
 		
 		} while (unit != null && unit.isType(LexType.CHAR) && !unit.isType(LexType.CTL) && !unit.isType(LexType.SEPARATOR));
 					
-		//TODO rollback
+		scanner.rollback(1);
 		
 		if (units.isEmpty()) {
 			return null;
