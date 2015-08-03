6ba339fa35708e44ed55b3385538f49308dc3ca0
Adam Paulik <error0x1e7@gmail.com>
2015-08-03 13:15:13 +0200

    added MorfiaSetUp.java, MongoDB support

---
+++ src/main/java/cz/esc/iot/cloudservice/persistance/dao/MorfiaSetUp.java
@@ -0,0 +1,32 @@
+package cz.esc.iot.cloudservice.persistance.dao;
+
+import com.mongodb.MongoClient;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+public class MorfiaSetUp {
+
+    private static final Morphia morphia = new Morphia();
+
+    static {
+        // tell Morphia where to find your classes
+        // can be called multiple times with different packages or classes
+        morphia.mapPackage("cz.esc.iot.cloudservice.persistance.model");
+    }
+
+    // create the Datastore connecting to the default port on the local host
+    private static final Datastore datastore = morphia.createDatastore(new MongoClient( "localhost" , 27017 ), "mongo_test");
+
+    static {
+        datastore.ensureIndexes();
+    }
+
+    public static Morphia getMorphia() {
+        return morphia;
+    }
+
+    public static Datastore getDatastore() {
+        return datastore;
+    }
+
+}
