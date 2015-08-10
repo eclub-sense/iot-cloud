696e6b4bd91bb832970e08dfa25418fa86a0d149
Adam Paulik <error0x1e7@gmail.com>
2015-08-10 14:05:28 +0200

    added Homepage.java to Router

---
+++ src/main/java/cz/esc/iot/cloudservice/resources/Homepage.java
@@ -0,0 +1,29 @@
+package cz.esc.iot.cloudservice.resources;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+public class Homepage extends ServerResource {
+
+    @Get("html")
+    public String returnList() {
+        String fileName = "index.html";
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fileName),
+                    Charset.defaultCharset());
+            for (String line : lines) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+}
