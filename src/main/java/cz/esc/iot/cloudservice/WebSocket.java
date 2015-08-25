d07eeddbcab393f67b05c4253fcec048e300f590
mpaclik <mpaclik@seznam.cz>
2015-08-25 11:01:28 +0200

    change ips.

--- src/main/java/cz/esc/iot/cloudservice/WebSocket.java
+++ src/main/java/cz/esc/iot/cloudservice/WebSocket.java
@@ -92,7 +92,7 @@ public class WebSocket extends WebSocketAdapter {
     	// example url: "ws://127.0.0.1:1337/servers/1111/events?topic=photocell%2F3afc5cd4-755f-422d-8585-c8d526af8e85%2Fintensity"
     	for (MeasureValue value : typeInfo.getValues()) {
     		System.out.println(value);
-    		String url = "ws://192.168.200.19:1337/servers/" + message.getUuid() + "/" +"events?topic=" 
+    		String url = "ws://127.0.0.1:1337/servers/" + message.getUuid() + "/" +"events?topic=" 
     		    + sensor.getType() + "%2F" + message.getSensorId() + "%2F" + value.getName();
     		// TODO get first values without ws
     		System.out.println(url);
@@ -164,7 +164,7 @@ public class WebSocket extends WebSocketAdapter {
     		boolean zetta = false;
     		
     		System.out.println(ip);
-    		if (ip.equals("90.183.112.6")) { // zetta-cloud ip
+    		if (ip.equals("127.0.0.1")) { // zetta-cloud ip
     			zetta = true;
     			System.out.println("\n\n\nzetta-cloud\n\n\n");
     		}
