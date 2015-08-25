b509de5a8683e35d0fbd7fcd60be4a624252e326
mpaclik <mpaclik@seznam.cz>
2015-08-25 15:29:46 +0200

    listening on callback url

--- src/main/java/cz/esc/iot/cloudservice/resources/Homepage.java
+++ src/main/java/cz/esc/iot/cloudservice/resources/Homepage.java
@@ -6,15 +6,29 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.restlet.data.Form;
 import org.restlet.ext.oauth.AccessTokenServerResource;
 import org.restlet.ext.oauth.OAuthException;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 
 public class Homepage extends AccessTokenServerResource {
 
+	@Post
+	public void b() {
+		System.out.println(getRequest().getEntityAsText());
+		Form headers = (Form) getRequest().getAttributes().get("org.restlet.http.headers");
+		System.out.println(headers.getQueryString());
+	}
+	
 	@Get("html")
     public String returnList() throws OAuthException {		
 
+		System.out.println(getRequest().getEntityAsText());
+		Form headers = (Form) getRequest().getAttributes().get("org.restlet.http.headers");
+		System.out.println(headers.getQueryString());
+		
+		
         String fileName = "haha.html";
         StringBuilder sb = new StringBuilder();
 
