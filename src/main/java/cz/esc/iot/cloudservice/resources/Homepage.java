bccfb9d1e57ce9b6e9df6fe5606aa5628ec735c4
mpaclik <mpaclik@seznam.cz>
2015-08-26 10:31:44 +0200

    repairing server.

--- src/main/java/cz/esc/iot/cloudservice/resources/Homepage.java
+++ src/main/java/cz/esc/iot/cloudservice/resources/Homepage.java
@@ -6,29 +6,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.restlet.data.Form;
-import org.restlet.ext.oauth.AccessTokenServerResource;
 import org.restlet.ext.oauth.OAuthException;
 import org.restlet.resource.Get;
-import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
 
-public class Homepage extends AccessTokenServerResource {
-
-	@Post
-	public void b() {
-		System.out.println(getRequest().getEntityAsText());
-		Form headers = (Form) getRequest().getAttributes().get("org.restlet.http.headers");
-		System.out.println(headers.getQueryString());
-	}
+public class Homepage extends ServerResource {
 	
 	@Get("html")
     public String returnList() throws OAuthException {		
 
-		System.out.println(getRequest().getEntityAsText());
-		Form headers = (Form) getRequest().getAttributes().get("org.restlet.http.headers");
-		System.out.println(headers.getQueryString());
-		
-		
         String fileName = "haha.html";
         StringBuilder sb = new StringBuilder();
 
