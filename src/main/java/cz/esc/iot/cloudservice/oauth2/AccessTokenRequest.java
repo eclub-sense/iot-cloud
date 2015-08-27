9fbd4a3bc52c11365a7a2cbc21d8540685295464
mpaclik <mpaclik@seznam.cz>
2015-08-27 15:18:34 +0200

    debug

--- src/main/java/cz/esc/iot/cloudservice/oauth2/AccessTokenRequest.java
+++ src/main/java/cz/esc/iot/cloudservice/oauth2/AccessTokenRequest.java
@@ -9,8 +9,21 @@ public class AccessTokenRequest {
 	@Expose private String client_id;
 	
 	public AccessTokenRequest() {
-		
+		super();
 	}
+	
+	public void setGrant_type(String grant_type) {
+		this.grant_type = grant_type;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public void setClient_id(String client_id) {
+		this.client_id = client_id;
+	}
+
 	public String getGrant_type() {
 		return grant_type;
 	}
