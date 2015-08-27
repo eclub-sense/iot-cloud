ea81e29e3170511c0a5f62f76ae1219960764dd1
mpaclik <mpaclik@seznam.cz>
2015-08-27 15:11:23 +0200

    debug

--- src/main/java/cz/esc/iot/cloudservice/resources/UserRegistrator.java
+++ src/main/java/cz/esc/iot/cloudservice/resources/UserRegistrator.java
@@ -3,6 +3,7 @@ package cz.esc.iot.cloudservice.resources;
 import java.io.IOException;
 
 import org.json.JSONException;
+import org.restlet.data.MediaType;
 import org.restlet.ext.oauth.OAuthException;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Post;
@@ -25,35 +26,38 @@ public class UserRegistrator extends ServerResource {
 	
 	@Post("json")
 	public String newUser(Representation entity) throws IOException, OAuthException, JSONException {
-		System.out.println("entity:" +entity.getText());
-		
-		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-		AccessTokenRequest request = gson.fromJson(entity.getText(), AccessTokenRequest.class);
-		System.out.println("request: "+request);
-		
-		if (request.getGrant_type() == null || !request.getGrant_type().equals("authorization_code"))
-			return "{\n\"error\":\"Invalid grant type.\",\n\"code\":2\n}";
-		
-		if (request.getClient_id() == null || !request.getClient_id().equals("abeceda"))
-			return "{\n\"error\":\"Invalid client id.\",\n\"code\":3\n}";
-		
-		if (request.getCode() == null)
-			return "{\n\"error\":\"No code received.\",\n\"code\":3\n}";
-				
-		// exchange authorisation code for info about user from Google
-		GoogleUserInfo googleUser;
-		try {
-			googleUser = OAuth2.getGoogleUserInfoFromCode(request.getCode());
-		} catch (OAuthException e) {
-			return "{\n\"error\":\"Invalid code.\",\n\"code\":1\n}";
+		if (entity.getMediaType().isCompatible(MediaType.APPLICATION_JSON)) {
+			System.out.println("entity:" +entity.getText());
+			
+			Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+			AccessTokenRequest request = gson.fromJson(entity.getText(), AccessTokenRequest.class);
+			System.out.println("request: "+request);
+			
+			if (request.getGrant_type() == null || !request.getGrant_type().equals("authorization_code"))
+				return "{\n\"error\":\"Invalid grant type.\",\n\"code\":2\n}";
+			
+			if (request.getClient_id() == null || !request.getClient_id().equals("abeceda"))
+				return "{\n\"error\":\"Invalid client id.\",\n\"code\":3\n}";
+			
+			if (request.getCode() == null)
+				return "{\n\"error\":\"No code received.\",\n\"code\":3\n}";
+					
+			// exchange authorisation code for info about user from Google
+			GoogleUserInfo googleUser;
+			try {
+				googleUser = OAuth2.getGoogleUserInfoFromCode(request.getCode());
+			} catch (OAuthException e) {
+				return "{\n\"error\":\"Invalid code.\",\n\"code\":1\n}";
+			}
+	
+			// register user
+			UserEntity newUser = gson.fromJson(entity.getText(), UserEntity.class);
+			newUser.setEmail(googleUser.getEmail());
+			MorfiaSetUp.getDatastore().save(newUser);
+			
+			CloudToken token = OAuth2.generateToken();
+			return gson.toJson(token);
 		}
-
-		// register user
-		UserEntity newUser = gson.fromJson(entity.getText(), UserEntity.class);
-		newUser.setEmail(googleUser.getEmail());
-		MorfiaSetUp.getDatastore().save(newUser);
-		
-		CloudToken token = OAuth2.generateToken();
-		return gson.toJson(token);
+		return "{\n\"error\":\"Invalid request.\",\n\"code\":5\n}";
 	}
 }
