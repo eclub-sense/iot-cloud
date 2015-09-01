68d338bcc88eb9504704263f7782df39150aac2d
mpaclik <mpaclik@seznam.cz>
2015-09-01 17:50:11 +0200

    Edit API.json file.

--- src/main/bash/start_zetta_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Starts zetta-server for new user.
-
-mkdir zetta-servers/"$1"
-cp -r resources/generic-zetta-server/. zetta-servers/"$1"
-sed -i -e 's/>>PORT<</'$2'/g' zetta-servers/"$1"/server.js
-sed -i -e 's/>>SERVER_NAME<</'$1'/g' zetta-servers/"$1"/server.js
-cd zetta-servers/"$1"
-./node server.js
